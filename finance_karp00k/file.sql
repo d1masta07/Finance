15c37d780db130c6cc856684a6910273d15d8e99
d1masta07 <144098016+d1masta07@users.noreply.github.com>
2023-12-12 00:04:40 +0700

    Add files via upload

---
+++ finance_karp00k/file.sql
@@ -0,0 +1,1 @@
+INSERT INTO accounts (account_id, number_of_shares, price, symbol, name) VALUES (1, 2, 800, "gay" , "namegay");
---
+++ finance_karp00k/helpers.py
@@ -0,0 +1,88 @@
+import os
+import requests
+import urllib.parse
+import datetime
+import pytz
+import uuid
+import csv
+import re
+
+from flask import redirect, render_template, request, session
+from functools import wraps
+
+
+def apology(message, code=400):
+    """Render message as an apology to user."""
+    def escape(s):
+        """
+        Escape special characters.
+
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+            s = s.replace(old, new)
+        return s
+    return render_template("apology.html", top=code, bottom=escape(message)), code
+
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+def lookup(symbol):
+    """Look up quote for symbol."""
+
+    # Prepare API request
+    symbol = symbol.upper()
+    end = datetime.datetime.now(pytz.timezone("US/Eastern"))
+    start = end - datetime.timedelta(days=7)
+
+    # Yahoo Finance API
+    url = (
+        f"https://query1.finance.yahoo.com/v7/finance/download/{urllib.parse.quote_plus(symbol)}"
+        f"?period1={int(start.timestamp())}"
+        f"&period2={int(end.timestamp())}"
+        f"&interval=1d&events=history&includeAdjustedClose=true"
+    )
+
+    # Query API
+    try:
+        response = requests.get(url, cookies={"session": str(uuid.uuid4())}, headers={"User-Agent": "python-requests", "Accept": "*/*"})
+        response.raise_for_status()
+
+        # CSV header: Date,Open,High,Low,Close,Adj Close,Volume
+        quotes = list(csv.DictReader(response.content.decode("utf-8").splitlines()))
+        quotes.reverse()
+        price = round(float(quotes[0]["Adj Close"]), 2)
+        return {
+            "name": symbol,
+            "price": price,
+            "symbol": symbol
+        }
+    except (requests.RequestException, ValueError, KeyError, IndexError):
+        return None
+
+
+def usd(value):
+    """Format value as USD."""
+    return f"${value:,.2f}"
+
+def validate(password):
+    if len(password) < 8:
+        return (False, "dlinna parolya doljna bit' bol'she 8 simvolov")
+    if re.search('[0-9]',password) is None:
+        return (False, "Dobav' cifru pls")
+    if re.search('[A-Z]',password) is None:
+        return (False, "Bolshuu bukvu nado")
+    return (True, "")
